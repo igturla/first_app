927c95c3dc9df9537585a6a040f89f4de5be1897
Isaiah Turla <isaiahturla@glyphgames.com>
2013-06-06 14:36:06 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b0b28f8b4857fbb0b8b373a503421b66c2cfe428f2c35942f14901ef27b6ffaa99c884875e285a553015bde26193760d6039906526cbb9937368505fd87866a1'
